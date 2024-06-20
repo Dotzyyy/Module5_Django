626f85bff9f80c54e5e444c9e2970f3e3a4b2a59
David Sexton <davidsexton36@gmail.com>
2024-06-20 09:21:57 +0100

    Inbox displaying correctly /add css

--- private_message/views.py
+++ private_message/views.py
@@ -6,20 +6,21 @@ from .forms import MessageForm
 @login_required
 def inbox(request):
     privatemessages = PrivateMessage.objects.filter(recipient=request.user).order_by('-timestamp')
-    return render(request, 'private_message/inbox.html', {'messages': privatemessages})
+    return render(request, 'private_message/inbox.html', {'privatemessages': privatemessages})
 
 @login_required
 def sent_items(request):
     privatemessages = PrivateMessage.objects.filter(author=request.user).order_by('-timestamp')
-    return render(request, 'private_message/sent_items.html', {'messages': privatemessages})
+    return render(request, 'private_message/sent_items.html', {'privatemessages': privatemessages})
 
 @login_required
 def view_item(request, pk):
     privatemessage = get_object_or_404(PrivateMessage, pk=pk)
+    print(privatemessage)
     if privatemessage.recipient == request.user:
         privatemessage.is_read = True
         privatemessage.save()
-    return render(request, 'private_message/view_item.html', {'message': privatemessage})
+    return render(request, 'private_message/view_item.html', {'privatemessage': privatemessage})
 
 @login_required
 def send_item(request):
