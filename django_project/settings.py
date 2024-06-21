6c80c24f0f527fff344d9044e3dc052f90ac6bfe
David Sexton <davidsexton36@gmail.com>
2024-06-21 21:10:47 +0100

    pip updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -94,8 +94,9 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+database_url = os.environ.get("DATABASE_URL")
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(database_url)
     }
 
 
