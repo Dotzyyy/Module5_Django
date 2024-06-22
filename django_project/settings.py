630907f34c1661a3151639e7d505bfca695da915
David Sexton <davidsexton36@gmail.com>
2024-06-22 10:06:49 +0100

    media clear

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,9 +141,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
