de45510bc82df2fc63a4119bdf91d91ef3428b87
David Sexton <davidsexton36@gmail.com>
2024-06-24 21:15:15 +0100

    static clear

--- staticfiles/private_message/js/unread.1c1755693c22.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const inboxItem =document.querySelectorAll('.inbox-list-item');
-
-    inboxItem.forEach(function (message) {  // Detecting whether unread == True on the privateMessage model
-        if (message.getAttribute('data-unread') == 'True') {
-            message.classList.add('unread');
-        }
-    })
-})
